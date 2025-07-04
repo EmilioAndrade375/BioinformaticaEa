6178f55fff6f40fdd4434457f9b1ae4aa6587cf1
Emilio Andrade <emilio.andrade375@gmail.com>
2025-07-03 19:34:33 -0500

    Añadiendo el primer código del proyecto, en específico el gen mecA de la especie Staphylococcus Aureus

---
+++ Scripts/Codes.sh
@@ -0,0 +1,7 @@
+
+## Codes for the proyect 
+
+# In order to download a from the NCBI database use the following code:
+
+# Species: Staphylococcus epidermidis , gene: Methicillin/oxacillin resistance (MRSA). [mecA]
+./datasets download gene symbol mecA --taxon "Staphylococcus epidermidis" --filename mecA_Sepidermidis.zip
